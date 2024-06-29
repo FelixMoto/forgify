173eaa27af52bd2fddb5ead12ecd5a68df6e64b0
Hanzule <39030707+FelixMoto@users.noreply.github.com>
2024-06-29 22:07:56 +0200

    remove emojis and dashes from deckname

--- commands/utils.py
+++ commands/utils.py
@@ -1,4 +1,6 @@
 """import packages"""
+
+
 # %%
 from pathlib import Path
 import re
@@ -71,6 +73,9 @@ def get_decklist_from_url(url, verbose):
 
     # find the deck name
     deckname = driver.find_elements(By.CLASS_NAME, "deckheader-name")[0].text
+    # modify deckname to remove amojis and slashes, that could mess up the save directory
+    deckname = remove_emoji(deckname)
+    deckname = deckname.replace("/", " ")
 
     driver.quit()
 
@@ -179,5 +184,50 @@ def save_to_dck(decklist, commander, save_path, verbose) -> None:
     # save decklist as file
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(decklist)
+
+
+def remove_emoji(string):
+    """ removes emojis from a string. useful for the deck names, because somtimes peorple
+    put emojis in there.
+
+    reference:
+    https://gist.github.com/slowkow/7a7f61f495e3dbb7e3d767f97bd7304b
+    
+    Parameters
+    ----------
+    string : str
+        input string
+    
+    Returns
+    -------
+    emoji_pattern : str
+        modified string without emojis
+    """
+
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002500-\U00002BEF"  # chinese char
+                               u"\U00002702-\U000027B0"
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\ufe0f"  # dingbats
+                               u"\u3030"
+                               "]+", flags=re.UNICODE)
+
+    emoji_pattern =  emoji_pattern.sub(r'', string)
+
+    return emoji_pattern
+
 
 # %%
