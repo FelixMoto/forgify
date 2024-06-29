fb4deefa9a42f59ac3920c220e75db02090970e1
Hanzule <39030707+FelixMoto@users.noreply.github.com>
2024-06-29 02:35:13 +0200

    allow partner commanders to be detected properly; name deck after title on moxfield

--- commands/utils.py
+++ commands/utils.py
@@ -1,6 +1,7 @@
 """import packages"""
 # %%
 from pathlib import Path
+import re
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -22,8 +23,11 @@ def get_decklist_from_url(url, verbose):
     
     Returns
     -------
-    decklist : str
-        the decklist as a string
+    Deck : dict
+        containing the fields
+        Deckname - name of the deck from moxfield
+        Decklist - full string with all cards
+        N_Commanders - the number of commanders
     """
 
     # Set Chrome options for headless mode
@@ -38,6 +42,7 @@ def get_decklist_from_url(url, verbose):
     # Example: Open a website in headless Firefox
     driver.get(url)
 
+
     # wait for page to fully load
     # Wait until the page is fully loaded
     if verbose:
@@ -46,33 +51,49 @@ def get_decklist_from_url(url, verbose):
 
     wait.until(EC.presence_of_element_located((By.ID, "subheader-more")))
 
-    # click more button
-    driver.find_elements(By.ID, "subheader-more")[0].click()
-    driver.implicitly_wait(1)
-
-    # click on export
-    driver.find_elements(By.LINK_TEXT,"Export")[0].click()
-    driver.implicitly_wait(2)
+    # click Download button
+    driver.find_elements(By.LINK_TEXT, "Download")[0].click()
+    driver.implicitly_wait(30)
 
     # get text from text area containing the decklist
     text_area = driver.find_elements(By.CLASS_NAME, "form-control")[0]
     decklist_str = text_area.text
+
+    # check if one or two commanders are in the list
+    cmd_tag = driver.find_elements(By.CLASS_NAME, "d-inline-block")[1].text
+    # find all numbers in string
+    n_cmd = re.findall(r'\d+', cmd_tag)
+    # if we have numbers, take the first, otherwise assume we have just one commander
+    if n_cmd:
+        n_cmd = int(n_cmd[0])
+    else:
+        n_cmd = 1
+
+    # find the deck name
+    deckname = driver.find_elements(By.CLASS_NAME, "deckheader-name")[0].text
+
     driver.quit()
 
-    return decklist_str
+    # pack into dict
+    Deck = {
+        "deckname" : deckname,
+        "n_commanders" : n_cmd,
+        "decklist" : decklist_str,
+    }
+
+    return Deck
 
 
-def map_to_dck(decklist):
+def map_to_dck(Deck):
     """takes the single string returned from get_decklist_from_url
     and map that onto a decklist that can be read by forge
     
     Parameters
     ----------
-    decklist : str
-        the decklist as a single string
-    verbose - bool
-        if the decklist and the total number of cards should be
-        printed to the command line
+    decklist : dict
+        the output from the function get_decklist_from_url
+        expected to contain the deckname, the card list as a single string
+        and the number of commanders
         
     Returns
     -------
@@ -83,6 +104,11 @@ def map_to_dck(decklist):
 
     """
 
+    # unpack dict
+    deckname = Deck["deckname"]
+    decklist = Deck["decklist"]
+    n_cmd = Deck["n_commanders"]
+
     # split decklist into main and sideboard, if we have sideboard cards
     main_sideboard = decklist.split("\n\n")
     # split decklist into cards by the return str, ignore sideboard for now
@@ -91,7 +117,7 @@ def map_to_dck(decklist):
     # go through all cards and transform the string
     forge_list = []
     num_cards = []
-    for ii, element in enumerate(card_list):
+    for element in card_list:
         # get amount of that card
         amount, rest = element.split(" ", 1)
         # get card name and expansion
@@ -101,13 +127,11 @@ def map_to_dck(decklist):
         forge_list.append(amount.strip() + " " + "|".join([cardname.strip(),expansion.strip(),"1"]))
         num_cards.append(int(amount))
 
-        # we assume the first card to be the commander
-        if ii == 0:
-            commander_name = cardname
 
     # concatenate strings into decklist
-    header = f"[metadata]\nName={commander_name:s}\n[Commander]\n{forge_list[0]:s}\n"
-    main = "[Main]\n" + "\n".join([elements for elements in forge_list[1:]]) + "\n[Sideboard]"
+    commanders = "\n".join([elements for elements in forge_list[:n_cmd]])
+    header = f"[metadata]\nName={deckname:s}\n[Commander]\n{commanders:s}\n"
+    main = "[Main]\n" + "\n".join([elements for elements in forge_list[n_cmd:]]) + "\n[Sideboard]"
     deck_str = header + main
 
     # command line invert color and reset
@@ -122,7 +146,7 @@ def map_to_dck(decklist):
     print(f"{reverse}Number of cards{reset}")
     print(sum(num_cards), "\n")
 
-    return deck_str, commander_name
+    return deck_str, deckname
 
 
 def save_to_dck(decklist, commander, save_path, verbose) -> None:
@@ -155,3 +179,5 @@ def save_to_dck(decklist, commander, save_path, verbose) -> None:
     # save decklist as file
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(decklist)
+
+# %%
